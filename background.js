f479c44abc94be4196343c191f7b8847cfbc2895
Pinesharp <111013695+pyrosfere@users.noreply.github.com>
2024-02-24 10:05:00 -0300

    Fixed a bug involving the background numbers

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 function setRandomBackground() {
-  const randomNumber = Math.floor(Math.random() * 9) + 1;
+  const randomNumber = Math.floor(Math.random() * 8) + 1;
   document.body.style.backgroundImage = `url('background${randomNumber}.jpg')`;
 }
